1580d22d544e8581bf379c863083cbcdb3d6db08
NIregbulem <soccergirl0000123@gmail.com>
2018-11-30 15:19:12 -0800

    Simple Calculator

--- Source.cpp
+++ Source.cpp
@@ -2,75 +2,109 @@
 #include <string>
 using namespace std;
 
-int main() {
-	cout << "=================================" << endl;
-	cout << "Welcome to Madlibs!!" << endl;
-	cout << "=================================" << endl;
+int main() 
+{
 	cout << "=======================" << endl;
-	cout << "      S O N G S        " << endl;
+	cout << "     CALCULATOR        " << endl;
 	cout << "=======================" << endl;
-	string yes_no="yes";
+	string yes_no = "yes";
 	while (yes_no == "yes") {
 		int option = 0;
-		cout << "1) Ariana Grande - Thank You, Next " << endl;
-		cout << "2) The 1975 - It's Not Living (If It's Not With You) " << endl;
-		cout << "3) Marshmello - Happier" << endl;
-		cout << endl << endl << endl;
-		while (option < 1 || option > 3) {
-			cout << "Please Enter and option 1-3" << endl;
+		cout << "(1) Addition" << endl;
+		cout << "(2) Subtraction" << endl;
+		cout << "(3) Multiplication" << endl;
+		cout << "(4) Division" << endl;
+		cout << "(5) Compare" << endl;
+		cout << "(6) Random" << endl;
+		while (option < 1 || option > 6) {
+			cout << "Please Enter and option 1-6" << endl;
 			cin >> option;
 		};
 
 		cout << endl << endl;
-		string lyric;
-		string name;
-		string noun;
-		string adjective;
-		string body_part;
-		string adjective2;
+		int number1;
+		int number2;
+		int answer;
 
 		if (option == 1) {
-			// Chosen Song Lyric ("Thought I'd end up with ___ but he wasn't a ___")
-			cout << "You have selected Ariana Grande's - Thank You, Next " << endl;
-			cout << " Now enter a name " << endl;
-			cin >> name;
-			cout << "Now enter a noun " << endl;
-			cin >> noun;
-			cout << "Congrats!! You have successfully completed your first MADLIB" << endl;
-			cout << "Thought I'd end up with " << name << " but he wasn't a " << noun << endl;
-
+			// Chosen Option is Addition
+			cout << "You have selected Addition" << endl;
+			cout << " Now enter a number " << endl;
+			cin >> number1;
+			cout << "Now enter a second number " << endl;
+			cin >> number2;
+			answer = number1 + number2;
+			cout << "Answer: " << answer << endl;
 		}
 		else {
-			// Chosen Song Lyric ( "Distract my ___ from the __ news")
+			// Chosen option is Subtraction
 			if (option == 2) {
-				cout << "You have selected The 1975 - It's Not Living (If It's Not With You) " << endl;
-				cout << "Now enter a body part" << endl;
-				cin >> body_part;
-				cout << "Now enter an adjective" << endl;
-				cin >> adjective;
-				cout << "Congrats!! You have successfully completed your first MADLIB" << endl;
-				cout << "Distract my " << body_part << " from the " << adjective << " news" << endl;
+				cout << "You have selected Subtraction " << endl;
+				cout << "Now enter a number" << endl;
+				cin >> number1;
+				cout << "Now enter a second number" << endl;
+				cin >> number2;
+				answer = number1 - number2;
+				cout << "Answer: " << answer << endl;
+
 			}
 			else {
-				// Chose Song Lyric ( "Lately, I've Been __, I want you to be ___")
+				// Chosen option is Multiplication
 				if (option == 3) {
-					cout << "You have selected Marshmello - Happier " << endl;
-					cout << "Now enter a adjective" << endl;
-					cin >> adjective;
-					cout << "Now enter an adjective" << endl;
-					cin >> adjective2;
-					cout << "Congrats!! You have successfully completed your first MADLIB" << endl;
-					cout << "Lately, I've been " << adjective << ", I want you to be " << adjective2 << endl;
-				}
-			}
-		}
-		cout << "Enter yes to play again" << endl;
-		cin >> yes_no;
-	};
-	
+					cout << "You have selected Multiplication " << endl;
+					cout << "Now enter a number" << endl;
+					cin >> number1;
+					cout << "Now enter a second number" << endl;
+					cin >> number2;
+					answer = number1 * number2;
+					cout << "Answer: " << answer << endl;
 
+				}
+				else {
+					// Chosen option is Division
+					if (option == 4) 
+					{
+						cout << "You have selected Division " << endl;
+						cout << "Now enter a number" << endl;
+						cin >> number1;
+						cout << "Now enter a second number" << endl;
+						cin >> number2;
+						answer = number1 / number2;
+						cout << "Answer: " << answer << endl;
+
+					}
+					else {
+						// Chosen option is Compare
+						if (option == 5) {
+							cout << "You have selected Compare " << endl;
+							cout << "Now enter a number " << endl;
+							cin >> number1;
+							cout << "Now enter a second number " << endl;
+							cin >> number2;
+							if (number1 < number2) {
+								cout << number1 << " is less than " << number2 << endl;
+							}
+							else {
+								cout << number1 << " is greater than " << number2 << endl;
+								}
+							}
+						}
+						
+					}
+
+
+
+
+				}
+
+
+			}
+			cout << "Enter yes to play again" << endl;
+			cin >> yes_no;
+		};
 	char exit;
 	cin >> exit;
 	return 0;
-}
+	}
+
 
